2311a1a71d5815e001ba4ac4791bb81cda19748f
Nikita <r1ck54nchez@yandex.ru>
2024-03-15 20:35:06 +0300

    product card

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Главная</a>
+    </div>
+</nav>
+
+<div class="container">
+    <slot></slot>
+</div>
+    
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
